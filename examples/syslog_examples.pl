bd0316d5ee5f5953b810e6e5a545f41cddb7bc9f
root <root@localhost.localdomain>
2016-12-11 09:48:22 +0000

    Adds working tc json extraction from rsyslog example.

--- examples/syslog_examples.pl
+++ examples/syslog_examples.pl
@@ -9,8 +9,13 @@ use Sys::Syslog qw(:standard :extended :macros);
 # ndelay - Open the connection immediately (normally, the connection is opened when the first message is logged).
 # pid - Include PID with each message.
 
+my $szJsonExampleFirst = "{ \"Version\": \"0.1.0\", \"TestCase\": \"my_super_duper_test_case\"}";
+
   setlogsock("udp");
   openlog("systelogtest", "ndelay,pid", LOG_LOCAL0);
 
-  syslog(LOG_INFO, "Message"); 
+  # syslog(LOG_INFO, "This is INFO"); 
+  # syslog(LOG_WARNING, "This is warning"); 
+print "$szJsonExampleFirst\n";
+  syslog(LOG_INFO, "$szJsonExampleFirst"); 
   closelog();
